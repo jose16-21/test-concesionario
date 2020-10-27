bf43fe1259b25d01c5bc11f5a6150f2c89b4338b
Juan Jose Hernandez <jjhernandez@bdgsa.net>
2020-10-26 23:00:14 -0600

    JUANJO: swagger add ok

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 var express = require("express"),
     router = express.Router();
-//var swaggerUi = require("swagger-ui-express");
-//var swaggerDocument = require("./../../swagger.json");
+var swaggerUi = require("swagger-ui-express");
+var swaggerDocument = require("./../../swagger.json");
 
-//router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));/
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /** Module Administrations**/
 const usuario = require("./AD/r_usuario");
@@ -18,14 +18,14 @@ const municipio = require("./TC/r_municipio");
 const persona = require("./TC/r_persona");
 const vehiculo = require("./TC/r_vehiculo");
 
-router.use('/usuario', usuario);
-router.use('/empresa', empresa);
+router.use('/usuarios', usuario);
+router.use('/empresas', empresa);
 
 router.use('/cotizacion', cotizacion);
 router.use('/departamento', departamento);
 router.use('/localizador', localizador);
 router.use('/municipio', municipio);
-router.use('/persona', persona);
+router.use('/personas', persona);
 router.use('/vehiculo', vehiculo);
 
 module.exports = router;
